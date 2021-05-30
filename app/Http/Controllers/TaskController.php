ee3b96537ad2cf150f6d6d0d952342b6134ee4ae
akoof <papay1000@gmail.com>
2021-05-30 18:47:20 +0300

    middleware forceJsonResponse

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -14,7 +14,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return Task::with('Tags');
+        return Task::with('Tags')->get();
     }
 
     /**
